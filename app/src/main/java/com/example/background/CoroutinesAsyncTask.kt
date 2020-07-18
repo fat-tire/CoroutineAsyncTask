34be3311a66f6c2b3ca374acd7e5754bcdc28d10
fattire <f4ttire@gmail.com>
2020-07-17 21:24:31 -0700

    Update CouroutinesAsyncTask/MainActivity
    To CouroutinesAsyncTask:
    
    * Add annotations / suppress lint warnings
    * Support cancel() & OnCanceled() hopefully
    * Use protected methods as did original
    * Remove Logging
    * Add license info to header
    * rename some variables to match ASyncTask()
    
    To MainActivity:
    
    * Added license info to header
    * Slight cleanup

--- app/src/main/java/com/example/background/CoroutinesAsyncTask.kt
+++ app/src/main/java/com/example/background/CoroutinesAsyncTask.kt
@@ -1,28 +1,68 @@
-package com.example.background
+/*
+ * Copyright (C) 2020 Rahul Lad
+ * Additional changes (C) 2020 by fat-tire same license below
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package  com.example.background;
 
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
 import com.example.background.Constant.Status
+import kotlinx.coroutines.*
 
-abstract class CoroutinesAsyncTask<Params, Progress, Result>{
+
+@MainThread
+abstract class CoroutinesAsyncTask<Params, Progress, Result> {
 
     var status: Status = Status.PENDING
-    abstract fun doInBackground(vararg params: Params?): Result
-    open fun onProgressUpdate(vararg values: Progress?) {}
-    open fun onPostExecute(result: Result?) {}
-    open fun onPreExecute() {}
-    open fun onCancelled(result: Result?) {}
-    protected var isCancelled = false
+    var isCancelled: Boolean = false
+    private val job: Job = Job()
+    val result = null
 
-    fun execute(vararg params: Params){
+    @WorkerThread
+    protected abstract fun doInBackground(vararg params: Params?): Result
+
+    @MainThread
+    protected open fun onProgressUpdate(vararg values: Progress?) {
+    }
+
+    @MainThread
+    protected open fun onPostExecute(result: Result?) {
+    }
+
+    @MainThread
+    protected open fun onPreExecute() {
+    }
+
+    @MainThread
+    protected open fun onCancelled(result: Result?) {
+        onCancelled()
+    }
+
+    @MainThread
+    protected open fun onCancelled() {
+    }
+
+    @MainThread
+    fun execute(vararg params: Params): CoroutinesAsyncTask<Params, Progress, Result> {
 
         if (status != Status.PENDING) {
-            when (status) {
-                Status.RUNNING -> throw IllegalStateException("Cannot execute task:" + " the task is already running.")
-                Status.FINISHED -> throw IllegalStateException("Cannot execute task:"
+            if (status == Status.RUNNING) {
+                throw IllegalStateException("Cannot execute task:"
+                        + " the task is already running.")
+            } else if (status == Status.FINISHED) {
+                throw IllegalStateException("Cannot execute task:"
                         + " the task has already been executed "
                         + "(a task can be executed only once)")
             }
@@ -31,37 +71,44 @@ abstract class CoroutinesAsyncTask<Params, Progress, Result>{
         status = Status.RUNNING
 
         // it can be used to setup UI - it should have access to Main Thread
-        GlobalScope.launch(Dispatchers.Main){
+        GlobalScope.launch(Dispatchers.Main) {
             onPreExecute()
         }
 
-        // doInBackground works on background thread(default)
-        GlobalScope.launch(Dispatchers.Default){
-            val result = doInBackground(*params)
-            status = Status.FINISHED
-            withContext(Dispatchers.Main){
-                // onPostExecute works on main thread to show output
-                Log.d("Alpha","after do in back "+status.name+"--"+isCancelled)
-                if (!isCancelled){onPostExecute(result)}
+        job.invokeOnCompletion {
+            if (it is CancellationException) {
+                isCancelled = true
+                GlobalScope.launch(Dispatchers.Main) {
+                    // note unless this is refactored, onCancelled will return null always
+                    onCancelled(null)
+                }
             }
         }
-    }
-
-    fun cancel(mayInterruptIfRunning : Boolean){
-        isCancelled = true
-        status = Status.FINISHED
-        GlobalScope.launch(Dispatchers.Main){
-            // onPostExecute works on main thread to show output
-            Log.d("Alpha","after cancel "+status.name+"--"+isCancelled)
-            onPostExecute(null)
+        // doInBackground works on background thread(default)
+        CoroutineScope(Dispatchers.Default + job).launch {
+            val result = doInBackground(*params)
+            status = Status.FINISHED
+            withContext(Dispatchers.Main) {
+                if (!job.isCancelled && !isCancelled) {
+                    onPostExecute(result)
+                }
+            }
         }
+        return this
     }
 
-    fun publishProgress(vararg progress: Progress) {
+    fun cancel(@Suppress("UNUSED_PARAMETER") mayInterruptIfRunning: Boolean): Boolean {
+        job.cancel()
+        isCancelled = true
+        return job.isCancelled
+    }
+
+    @WorkerThread
+    fun publishProgress(vararg values: Progress) {
         //need to update main thread
-        GlobalScope.launch(Dispatchers.Main){
-            if (!isCancelled){
-                onProgressUpdate(*progress)
+        GlobalScope.launch(Dispatchers.Main) {
+            if (!job.isCancelled) {
+                onProgressUpdate(*values)
             }
         }
     }
--- app/src/main/java/com/example/background/MainActivity.kt
+++ app/src/main/java/com/example/background/MainActivity.kt
@@ -1,12 +1,27 @@
+/*
+ * Copyright (C) 2020 Rahul Lad
+ * Additional changes (C) 2020 by fat-tire same license below
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.background
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.background.Constant.Status
 import kotlinx.android.synthetic.main.activity_main.*
-import com.example.background.R
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,16 +40,22 @@ class MainActivity : AppCompatActivity() {
         task?.execute(10)
     }
 
+    fun cancelIt(v: View) {
+        if (task?.status == Status.RUNNING) {
+            task?.cancel(true)
+        }
+    }
+
     class MyAsyncTask(private var activity: MainActivity?) : CoroutinesAsyncTask<Int, Int, String>() {
 
         override fun doInBackground(vararg params: Int?): String {
-            for (count in 1..10){
+            for (count in 1..10) {
                 if (isCancelled)
                     break
                 try {
                     Thread.sleep(1000)
                     publishProgress(count)
-                } catch (e: InterruptedException){
+                } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
             }
@@ -48,16 +69,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onPreExecute() {
-            activity?.output?.setText("Tast starting..")
+            activity?.output?.text = "Test starting..."
             activity?.progressBar?.visibility = View.VISIBLE
             activity?.progressBar?.max = 10
             activity?.progressBar?.progress = 0
+            activity?.cancelBtn?.visibility = View.VISIBLE
 
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
             activity?.output?.setText("count is ${values.get(0).toString()}")
-            values[0]?.let { activity?.progressBar?.setProgress(it) }
+            values[0]?.let {
+                activity?.progressBar?.setProgress(it)
+            }
+        }
+
+        override fun onCancelled(result: String?) {
+            activity?.output?.setText("Cancelled!")
+            activity?.cancelBtn?.visibility = View.GONE
         }
     }
 }
